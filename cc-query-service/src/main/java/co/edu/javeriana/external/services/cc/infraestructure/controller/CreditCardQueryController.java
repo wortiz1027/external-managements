426ae2fbd5b979bc014056116d39c5a57a5ed689
Wilman Ortiz <willman.ortiz@outlook.com>
2020-11-18 07:42:09 -0500

    Agrega configuraciones de documentacion y dockers

--- cc-query-service/src/main/java/co/edu/javeriana/external/services/cc/infraestructure/controller/CreditCardQueryController.java
+++ cc-query-service/src/main/java/co/edu/javeriana/external/services/cc/infraestructure/controller/CreditCardQueryController.java
@@ -4,6 +4,10 @@ import co.edu.javeriana.external.services.cc.application.CreditCardServices;
 import co.edu.javeriana.external.services.cc.domain.Status;
 import co.edu.javeriana.external.services.cc.dtos.Request;
 import co.edu.javeriana.external.services.cc.dtos.Response;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +20,18 @@ import java.util.concurrent.ExecutionException;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Api(value="Gestion de tarjetas de credito de los clientes de toures balon")
 public class CreditCardQueryController {
 
     private final CreditCardServices service;
 
+    @ApiOperation(value = "Consulta informacion de la tarjeta de credito reportada por los clientes de toures balon", response = Response.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Consulta exitosa de tarjeta de credito"),
+            @ApiResponse(code = 400, message = "Error en los datos de entrada no se envio informacion"),
+            @ApiResponse(code = 404, message = "Error no se encontro informacion de la tarjeta de credito reportada"),
+            @ApiResponse(code = 500, message = "Error interno en el servidor, contacte y reporte con el administrador")
+    })
     @GetMapping(value = "/creditCardValidation/{number}")
     public ResponseEntity<CompletableFuture<Response>> creditCardValidation(@PathVariable String number) throws ExecutionException, InterruptedException, UnknownHostException {
 
